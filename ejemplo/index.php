a19d0793d93257fa1685967a8fe69c9340167c41
Bryan Zavala <bzavalavaldivia@gmail.com>
2018-12-14 13:38:44 -0500

    Initial commit

---
+++ ejemplo/index.php
@@ -0,0 +1,5 @@
+<?php
+include __DIR__ . "/../vendor/autoload.php";
+
+$total = new bryanzavala\helpers\TotalEnLetras();
+$total->convertir(1250.58, 'SOLES'); //MIL DOSCIENTOS CINCUENTA Y 58/100 SOLES
